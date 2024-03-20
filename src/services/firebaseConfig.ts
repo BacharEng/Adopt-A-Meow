af4814aacd98c4cfbe996cb8553213baf52d5848
Sagi Bachar Adler <sagienator@gmail.com>
2024-03-20 19:30:32 +0200

    שדג

---
+++ src/services/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvDGOeDMlc-phT291-alxRqIVMWgWAQKE",
+  authDomain: "adopt-a-meow.firebaseapp.com",
+  projectId: "adopt-a-meow",
+  storageBucket: "adopt-a-meow.appspot.com",
+  messagingSenderId: "813285627452",
+  appId: "1:813285627452:web:0560082b05003d84ae1539",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
